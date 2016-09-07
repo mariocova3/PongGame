efe3122d6e8428ebc20c7996dbb341c1249b1ce4
mariocova3 <mariocova3@gmail.com>
2016-09-07 13:30:08 -0500

    Initial Commit
    Including all files I had completed before uploading to GitHub.

---
+++ Assets/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    public static int PlayerScore1 = 0;
+    public static int PlayerScore2 = 0;
+
+    public GUISkin layout;
+
+    Transform theBall;
+
+	// Use this for initialization
+	void Start ()
+    {
+        theBall = GameObject.FindGameObjectWithTag("Ball").transform;
+	}
+
+    public static void Score(string wallID)
+    {
+        if (wallID == "rightWall")
+        {
+            PlayerScore1++;
+        }
+        else
+        {
+            PlayerScore2++;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
---
+++ Assets/HUD.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour
+{
+    public GameObject Player1;
+    public GameObject Player2;
+
+    public BoxCollider2D topWall;
+    public BoxCollider2D bottomWall;
+    public BoxCollider2D leftWall;
+    public BoxCollider2D rightWall;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
